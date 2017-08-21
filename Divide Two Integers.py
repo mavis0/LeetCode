f026417ebb184c7caf4e0d801ef540464201c7af
mavis0 <646357406@qq.com>
2017-08-21 23:10:34 +0800

    routine

---
+++ Divide Two Integers.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        #O(n)，对于11111111111和1来说太慢了
+        # flag = True if (divisor > 0 and dividend > 0) or (divisor < 0 and dividend < 0) else False
+        # dividend, divisor = abs(dividend), abs(divisor)
+        # if divisor == 0: return -1
+        # i = 0
+        # while dividend > divisor:
+        #     dividend -= divisor
+        #     i += 1
+        # return i if flag else -i
+        #
+        flag = True if (divisor > 0 and dividend > 0) or (divisor < 0 and dividend < 0) else False
+        dividend, divisor = abs(dividend), abs(divisor)
+        ans = 0
+        while dividend >= divisor:
+            d = 0
+            while divisor << d <= dividend:
+                d += 1
+            ans += 1 << (d-1)
+            dividend -= divisor << (d-1)
+
+        return ans
+
+
+
+
+
+s = Solution()
+print(s.divide(2147483648, 1))
