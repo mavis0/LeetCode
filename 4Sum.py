89d15f4eedb6bd0c32457e3923247fd1762ce0cd
mavis0 <646357406@qq.com>
2017-09-03 18:10:26 +0800

    modify some file

--- 4Sum.py
+++ 4Sum.py
@@ -5,39 +5,70 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        nums.sort()
-        l, i, rm = [], 0, []
-        while i < len(nums) - 3:
-            n = i + 1
-            while n < len(nums) - 2:
+        #4sum 退化成 3sum，再退化成2sum，整体的复杂度是O(n^3)，在leetcode上的成绩是979 ms，36%
+        # nums.sort()
+        # l, i, rm = [], 0, []
+        # while i < len(nums) - 3:
+        #     n = i + 1
+        #     while n < len(nums) - 2:
                 
-                ln = nums[n]
-                start, end = n+1, len(nums)-1
-                t = nums[i] + nums[n] + nums[start] + nums[end]
-                while start < end:
-                    if t < target:
-                        start += 1
-                    elif t > target:
-                        end -= 1
-                    else:
-                        tmp = [nums[i], nums[n], nums[start], nums[end]]
-                        if rm != tmp:
-                            l.append(tmp)
-                        rm = tmp
-                        start += 1
-                        end -= 1
-                    t = nums[i] + nums[n] + nums[start] + nums[end]
-                while nums[n] == nums[n+1] and n < len(nums) - 2:
-                    n += 1
-                n += 1
+        #         ln = nums[n]
+        #         start, end = n+1, len(nums)-1
+        #         t = nums[i] + nums[n] + nums[start] + nums[end]
+        #         while start < end:
+        #             if t < target:
+        #                 start += 1
+        #             elif t > target:
+        #                 end -= 1
+        #             else:
+        #                 tmp = [nums[i], nums[n], nums[start], nums[end]]
+        #                 if rm != tmp:
+        #                     l.append(tmp)
+        #                 rm = tmp
+        #                 start += 1
+        #                 end -= 1
+        #             t = nums[i] + nums[n] + nums[start] + nums[end]
+        #         while nums[n] == nums[n+1] and n < len(nums) - 2:
+        #             n += 1
+        #         n += 1
             
-            while nums[i] == nums[i+1] and i < len(nums) - 3:
-                i += 1
-            i += 1
-        return l
+        #     while nums[i] == nums[i+1] and i < len(nums) - 3:
+        #         i += 1
+        #     i += 1
+        # return l
+        #尝试一下用hashtable的办法，应该可以将算法复杂度降到O(n^2)，分成两个2sum，然后再进行相加
+        #但是去重好像都是用的set， 没有什么更好的办法，可能效果还不如退化成3sum，所以，就没有再往下写
+        nums.sort()
+        a, d, n = set(), {}, len(nums)
+        for i in range(n - 1):
+            j = i + 1
+            while j < n:
+                s = nums[i] + nums[j]
+                if s not in d.keys():
+                    d[s] = [[i, j]]
+                else:
+                    d[s].append([i, j])
+                j += 1
 
-            
+        for i in range(n-3):
+            for j in range(i+1, n-2):
+                r = target - nums[i] - nums[j]
+                if r in d.keys():
+                    for k in d[r]:
+                        if k[0] > j:
+                            a.add((nums[i],nums[j], nums[k[0]], nums[k[1]]))
+        return [list(i) for i in a]
+
+        # for i in d.keys():
+        #     if target - i in d.keys():
+        #         for m in d[i]:
+        #             for n in d[target-i]:
+        #                 if m[1] < n[0]:
+        #                     tmp = m.copy()
+        #                     tmp.extend(n)
+        #                     a.append(tmp)
+        
 
 
 s = Solution()
-print(s.fourSum([0, 0, 0, 0], 0))
+print(s.fourSum([-1,2,2,-5,0,-1,4], 3))
--- Implement strStr().py
+++ Implement strStr().py
@@ -21,32 +21,47 @@ class Solution(object):
         #         i += 1
         
         # return i if i != len(haystack) else -1
-        #用KMP试一试，第一版，但是也只是9.22%，也不应该啊，再试试吧
-        j, k = 0, -1
+        #用KMP试一试，第一版，但是也只是9.22%，也不应该啊，再试试吧，看了别人的submit，都反映kmp
+        #比暴力算法要慢，可能测试用例有问题吧
+        # if not needle: return 0
+        # j, k = 0, -1
+        # h, n = len(haystack), len(needle)
+        # nx = [-1] * n
+        # while j < n - 1:
+        #     if k == -1 or needle[j] == needle[k]:
+        #         k += 1
+        #         j += 1
+        #         nx[j] = k
+        #     else:
+        #         k = nx[k]
+
+        # i, j = 0, 0
+        # while i < h - n + 1:
+        #     while i < h and j < n and haystack[i] == needle[j]:
+        #         i, j = i+1, j+1
+
+        #     if j == n:
+        #         return i - j
+        #     elif j == 0:
+        #         i += 1
+        #     else:
+        #         j = nx[j]
+
+        # return -1
+        #看过别人的代码后，再次尝试用暴力算法，11%的成绩太难看
+        if not needle: return 0
+        i = 0
         h, n = len(haystack), len(needle)
-        nx = [-1] * n
-        while j < n - 1:
-            if k == -1 or needle[j] == needle[k]:
-                k += 1
-                j += 1
-                nx[j] = k
-            else:
-                k = nx[k]
-
-        i, j = 0, 0
-        while i < h:
-            while i < h and j < n and haystack[i] == needle[j]:
-                i, j = i+1, j+1
-
-            if j == n:
-                return i - j
-            elif j == 0:
-                i += 1
-            else:
-                j = nx[j]
-
+        while i <= h - n:
+            for j in range(n):
+                if haystack[i+j] != needle[j]:
+                    i += 1
+                    break
+                if j == n - 1:
+                    return i
         return -1
-
+        #24%,55ms还是太慢啊，但是Google了一圈也没有找到更快的python代码，
+        #哼，他们肯定都是直接用的find函数！
 
 s = Solution()
-print(s.strStr("mississippi", "a"))
+print(s.strStr("", "as"))
