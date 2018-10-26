d6b2ed89baa118c60c1b723c3bd67a94ad94c3a6
mavis0 <646357406@qq.com>
2018-10-26 22:53:37 +0800

    Populating Next Right Pointers in Each Node

--- Populating Next Right Pointers in Each Node.py
+++ Populating Next Right Pointers in Each Node.py
@@ -9,4 +9,11 @@ class Solution:
     # @param root, a tree link node
     # @return nothing
     def connect(self, root):
-        pass
+        if not root: return
+        if root.left and root.right:
+            tmp1, tmp2 = root.left, root.right
+            while tmp1 and tmp2:
+                tmp1.next = tmp2
+                tmp1, tmp2 = tmp1.right, tmp2.left
+            self.connect(root.left)
+            self.connect(root.right)
